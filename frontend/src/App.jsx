bcf1793219fb40d79ba32e9eededb2948dd963d4
Marius Van Staden <mvstaden91@gmail.com>
2025-02-18 03:56:43 +0200

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
